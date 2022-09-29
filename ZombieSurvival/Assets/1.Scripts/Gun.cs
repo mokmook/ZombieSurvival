40ac33a17b9437249f56d3b37d64e4e3c32973f2
mokmook <whdnfl0504@naver.com>
2022-09-29 10:48:01 +0900

    220929

--- ZombieSurvival/Assets/1.Scripts/Gun.cs
+++ ZombieSurvival/Assets/1.Scripts/Gun.cs
@@ -47,27 +47,80 @@ public class Gun : MonoBehaviour
     }  
     public void Fire()
     {
-      
+        if (state==State.Ready&&Time.time>=lastFireTime+gunData.timeBetFire)
+        {
+            lastFireTime = Time.time;
+            Shot();
+        }
     }
     private void Shot()
     {
+        Debug.DrawRay(fireTransform.position, fireTransform.forward, color: Color.red);
 
+        RaycastHit hit;
+
+        Vector3 hitPos;
+        if (Physics.Raycast(fireTransform.position, fireTransform.forward, out hit,fireDistance))
+        {
+            IDamageable target = hit.collider.GetComponent<IDamageable>();
+            
+            if (target != null)
+                target.OnDamage(gunData.damage, hit.point, hit.normal);
+
+            hitPos = hit.point;
+        }
+        
+        else       
+            hitPos = fireTransform.position + fireTransform.forward * fireDistance;
+
+        StartCoroutine("ShotEffect", hitPos);
+        
+
+        magAmmo--;
+
+        if (magAmmo <= 0)
+            state = State.Empty;
     }
 
     private IEnumerator ShotEffect(Vector3 hitPos)
     {
+        muzzleFlashEffect.Play();
+        shellEjectEffect.Play();
+
+        gunAuidoPlayer.PlayOneShot(gunData.shotClip);
+
+        bulletLineRenderer.SetPosition(0, fireTransform.position);
+        bulletLineRenderer.SetPosition(1, hitPos);
         bulletLineRenderer.enabled = true;
+
         yield return new WaitForSeconds(0.03f);
-        bulletLineRenderer.enabled = false;
+
+        bulletLineRenderer.enabled = false; 
     }
+    
     public bool Reload()
-    {
+    {      
+        if(state==State.Reloading||ammoRemain<=0||magAmmo>=gunData.magCapacity)
         return false;
+
+        StartCoroutine("ReloadRoutine");
+        return true;
     }
     private IEnumerator ReloadRoutine()
     {
         state = State.Reloading;
+        gunAuidoPlayer.PlayOneShot(gunData.reloadClip);
+
         yield return new WaitForSeconds(gunData.reloadTime);
+
+        int ammoToFill = gunData.magCapacity - magAmmo;
+
+        if (ammoRemain < ammoToFill)
+            ammoToFill = ammoRemain;
+
+        magAmmo += ammoToFill;
+        ammoRemain -= ammoToFill;
+
         state = State.Ready;
     }
     
